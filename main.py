92c7ba2f966e889e75eb3acb2119d3fd3d04731a
Chi Man Lei <chimanlei@gmail.com>
2026-02-02 12:20:14 +0800

    Implement configurable greeting, generic 'app' entrypoint, and Procfile

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from fastapi import FastAPI
 import sys
+import os
 from version import __version__
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
+    greeting = os.environ.get("GREETING", "Hello")
     return {
-        "message": "Hello from Luban CI Sample App!",
+        "message": f"{greeting} from Luban CI Sample App!",
         "python_version": sys.version,
         "version": __version__,
     }
 
-if __name__ == "__main__":
+
+def start():
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
+
+if __name__ == "__main__":
+    start()
